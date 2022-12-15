50ce331f623c612852f06ed97226314aa7ec252f
Bismarck <bismarckdados@gmail.com>
2022-12-15 10:24:06 -0300

    feat: config database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express, { json } from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
 app.use(json);
 app.use(cors);
 app.use(router);
 
-app.listen(5000, () => console.log("Conectado na porta 5000"));
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Conectado na porta ${port}`));
